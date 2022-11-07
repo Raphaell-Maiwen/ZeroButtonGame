fbb70ef7e9fe0b24e15f3b37c7c4d49d578af7df
Raphaëll <raphaelle.mp@gmail.com>
2022-11-07 16:11:45 -0500

    Refactoring VS1

--- Assets/2D Hardocre Runner Kit v2.0/Scripts/Game/HorizontalSaw.cs
+++ Assets/2D Hardocre Runner Kit v2.0/Scripts/Game/HorizontalSaw.cs
@@ -9,33 +9,29 @@ public class HorizontalSaw : MonoBehaviour
 
     bool ColliderActive = true;
 
-    private void Start()
+    /*private void Start()
     {
-        //Debug.Log("Starting Saw");
         MovingObstacle movingObstacle = SawToFollow.gameObject.GetComponent<MovingObstacle>();
         movingObstacle.updateSawTriggers.Add(UpdateSawTrigger);
 
-        /*if (movingObstacle.point == 1)
-        {
-            jumpCollider.GetComponent<BoxCollider2D>().enabled = false;
-            jumpCollider.GetComponent<SpriteRenderer>().enabled = false;
-            ColliderActive = false;
-        }
-        else
-        {*/
-            jumpCollider.GetComponent<BoxCollider2D>().enabled = true;
-            jumpCollider.GetComponent<SpriteRenderer>().enabled = true;
-            ColliderActive = true;
-        //}
+        jumpCollider.GetComponent<BoxCollider2D>().enabled = true;
+        jumpCollider.GetComponent<SpriteRenderer>().enabled = true;
+        ColliderActive = true;
     }
-
-    public void UpdateSawTrigger()
+    */
+    public void SwitchJump()
     {
-        /*Debug.Log("GettingCalled");
-        Debug.Log(ColliderActive);*/
         ColliderActive = !ColliderActive;
 
-        jumpCollider.GetComponent<BoxCollider2D>().enabled = ColliderActive;
-        jumpCollider.GetComponent<SpriteRenderer>().enabled = ColliderActive;
+        //jumpCollider.GetComponent<BoxCollider2D>().enabled = ColliderActive;
+        //jumpCollider.GetComponent<SpriteRenderer>().enabled = ColliderActive;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("SwitchJump"))
+        {
+            SwitchJump();
+        }
     }
 }
---
+++ Assets/2D Hardocre Runner Kit v2.0/Scripts/Game/SwitchJump.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchJump : MonoBehaviour
+{
+    public GameObject jumpCollider;
+    public bool turningOn;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            jumpCollider.GetComponent<BoxCollider2D>().enabled = turningOn;
+            jumpCollider.GetComponent<SpriteRenderer>().enabled = turningOn;
+        }
+    }
+}
